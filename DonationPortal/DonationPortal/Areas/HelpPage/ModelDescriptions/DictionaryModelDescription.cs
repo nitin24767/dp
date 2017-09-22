eb507025e36dfd80e3c2427ec273d4a89eed1b0d
Your Name <saki.nitin1985@gmail.com>
2017-09-22 19:26:17 +0530

    added project

---
+++ DonationPortal/DonationPortal/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace DonationPortal.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ DonationPortal/DonationPortal/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(DonationPortal.Startup))]
+
+namespace DonationPortal
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
